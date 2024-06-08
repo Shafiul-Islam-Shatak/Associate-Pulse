40a85eec7e5d9177d88e11e06e7f2d975d32de70
Shatak-the-imposter <helloshatak@gmail.com>
2024-06-08 21:16:39 +0600

    jwt verification done, without token 401 error and logout user

--- src/CustomHook/useAxiosSecure.jsx
+++ src/CustomHook/useAxiosSecure.jsx
@@ -1,10 +1,38 @@
 import axios from "axios";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../Provider/AuthProvider";
 
 const axiosSecure = axios.create({
     baseURL: import.meta.env.VITE_API_URL
 })
 const useAxiosSecure = () => {
-   return axiosSecure
+    const { logOut } = useContext(AuthContext)
+    const navigate = useNavigate()
+    axiosSecure.interceptors.request.use(function (config) {
+        const token = localStorage.getItem('access-token')
+        console.log('req stopped by interceptor', token);
+        config.headers.authorization = `Bearer ${token}`
+        return config
+    }, function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+    })
+
+    // 401 and 403
+    axiosSecure.interceptors.response.use(function (response) {
+        return response
+    }, async function (error) {
+        const status = error.response.status;
+        console.log('error by interceptor', status);
+        if (status === 401 || status === 403) {
+            await logOut()
+            navigate('/login')
+        }
+        return Promise.reject(error);
+    })
+
+    return axiosSecure
 };
 
 export default useAxiosSecure;
