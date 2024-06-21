04be4ce2e5b61b29991e72cf57c0240f8867c8d3
Shatak-the-imposter <helloshatak@gmail.com>
2024-06-22 01:07:35 +0600

    pagination for payment history page done

--- src/CustomHook/useAxiosSecure.jsx
+++ src/CustomHook/useAxiosSecure.jsx
@@ -23,7 +23,7 @@ const useAxiosSecure = () => {
     axiosSecure.interceptors.response.use(function (response) {
         return response
     }, async function (error) {
-        const status = error.response.status;
+        const status = error?.response?.status;
         // console.log('error by interceptor', status);
         if (status === 401 || status === 403) {
             await logOut()
--- src/Pages/Dashbord/Payment/PaymentHistory.jsx
+++ src/Pages/Dashbord/Payment/PaymentHistory.jsx
@@ -4,69 +4,104 @@ import useAxiosSecure from "../../../CustomHook/useAxiosSecure";
 import SectionTitle from "../../../Shared Components/SectionTitle";
 import { ClimbingBoxLoader } from "react-spinners";
 import { Helmet } from "react-helmet-async";
+import { useState } from "react";
 
 const PaymentHistory = () => {
     const axiosSecure = useAxiosSecure();
-    const user = useAuth()
-    const { data: payments = [] , isLoading} = useQuery({
-        queryKey: ['payments'],
-        queryFn: async () => {
-            const res = await axiosSecure.get(`/my-payment-history/${user.email}`);
-            return res.data;
+    const user = useAuth();
 
+    // Define state variables
+    const [iteamPerPage, setIteamPerPage] = useState(5);
+    const [currentPage, setCurrentPage] = useState(0);
+
+    const { data, isLoading } = useQuery({
+        queryKey: ['payments', iteamPerPage, currentPage],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/my-payment-history/${user.email}?page=${currentPage}&size=${iteamPerPage}`);
+            return res.data;
         }
-    })
-    // console.log(user);
+    });
+
+    const payments = data?.payments || [];
+    console.log(payments);
+    const totalCount = data?.totalCount || 0;
+    console.log(totalCount);
+
+    const numberOfPages = Math.ceil(totalCount / iteamPerPage);
+    const pages = [...Array(numberOfPages).keys()];
+
+    const handleIteamPerPage = (e) => {
+        const iteamCount = parseInt(e.target.value);
+        setIteamPerPage(iteamCount);
+        setCurrentPage(0);
+    };
+
+    const handlePrevPage = () => {
+        if (currentPage > 0) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
+
+    const handleNextPage = () => {
+        if (currentPage < pages.length - 1) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
+
     return (
         <div>
-            <div>
             <Helmet>
                 <title>Payment History</title>
             </Helmet>
-                <SectionTitle
-                    title={`Payment History of ${user?.displayName}`}
-                    description={`Total trasnsaction ${payments.length}`}
-                ></SectionTitle>
-            </div>
-
+            <SectionTitle
+                title={`Payment History of ${user?.displayName}`}
+                description={`Total transaction ${totalCount}`}
+            />
             <div className="overflow-x-auto">
-            {isLoading &&
-                <div className="flex justify-center items-center h-screen">
-                    <div className="w-16 h-16">
+                {isLoading ? (
+                    <div className="flex justify-center items-center h-screen">
                         <ClimbingBoxLoader color="#36d7b7" />
                     </div>
-                </div>
-            }
-                <table className="table">
-                    {/* head */}
-                    <thead>
-                        <tr>
-                            <th>
-                                SL
-                            </th>
-                            <th>Month</th>
-                            <th>Amount</th>
-                            <th>Transaction Id</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            payments.map((payment , index) =>
-                                <tr key={payment._id} >
-                                    <td>
-                                        {index + 1}
-                                    </td>
+                ) : (
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>SL</th>
+                                <th>Month</th>
+                                <th>Amount</th>
+                                <th>Transaction Id</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {payments.map((payment, index) => (
+                                <tr key={payment._id}>
+                                    <td>{index + 1}</td>
                                     <td>{payment.month}</td>
                                     <td>{payment.salary}</td>
                                     <td>{payment._id}</td>
                                 </tr>
-                            )
-                        }
-                        {/* row 1 */}
-
-                    </tbody>
-
-                </table>
+                            ))}
+                        </tbody>
+                    </table>
+                )}
+                <div className="justify-center flex mt-8">
+                    <button onClick={handlePrevPage} className="bg-slate-200 btn mr-2">Previous</button>
+                    {pages.map((page) => (
+                        <button
+                            className={`btn ${currentPage === page ? 'mx-1 bg-orange-400 text-2xl' : 'mx-1 bg-gray-200'}`}
+                            onClick={() => setCurrentPage(page)}
+                            key={page}
+                        >
+                            {page + 1}
+                        </button>
+                    ))}
+                    <button onClick={handleNextPage} className="bg-slate-200 btn ml-2">Next</button>
+                    <select value={iteamPerPage} onChange={handleIteamPerPage}>
+                        <option value="5">5</option>
+                        <option value="10">10</option>
+                        <option value="20">20</option>
+                    </select>
+                </div>
             </div>
         </div>
     );
